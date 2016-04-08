bf387341a7f362329aac03116133c2791ea08ce4
misha <misha.stepanov@mail.ru>
2016-04-08 22:55:11 +0300

    Настроил Angular в какой-то степени. Подустал прервусь

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,34 @@
-
+angular.module('Soshace', [])
+	.controller('mainCtrl', function($scope, $http){
+		$scope.candidates =[ 
+			{
+			name: "Никита Брагин",
+			time: "12-00",
+			place: "Офис"
+			},
+			{
+			name: "Павел Брагин",
+			time: "19-00",
+			place: "Офис"
+			},
+			{
+			name: "Денис Нефедорв",
+			time: "13-00",
+			place: "Скайп"
+			},
+			 {
+			name: "Марк Губарев",
+			time: "12-00",
+			place: "Скайп"
+			},
+		];
+		
+		$scope.submitData = function(){
+			alert('Данные получены');
+		}
+		
+		$scope.adminLogin =function(admin){
+			alert(admin.password);
+			$scope.adminLoggedIn =true;
+		};
+	});
